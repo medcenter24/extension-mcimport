bfea32db9ce7f5b7b357ef1f6bd5a027d52b6987
MedCenter24 Dev <dev@medcenter24.com>
2019-04-24 18:56:09 +0300

    fixed import

--- Services/CaseImporterService.php
+++ Services/CaseImporterService.php
@@ -4,6 +4,7 @@
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
  * of the License (non-upgradable).
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -11,18 +12,26 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
  * Copyright (c) 2019 (original work) MedCenter24.com;
  */
 
-namespace medcenter24\McImport\Contract;
+namespace medcenter24\McImport\Services;
 
 
 use medcenter24\mcCore\App\Accident;
+use medcenter24\McImport\Contract\CaseImporter;
+use medcenter24\McImport\Contract\CaseImporterProvider;
 
-interface CaseImporter
+class CaseImporterService implements CaseImporter
 {
-    public const DISC_IMPORTS = 'imports';
-    public const CASES_FOLDERS = 'cases';
+    public function import(string $path): Accident
+    {
+        // get Docx from the storage and run importer
+        $provider = resolve(CaseImporterProvider::class);
+        $provider->load($path);
+        $provider->import();
 
-    public function import(string $path): Accident;
+        return $provider->getLastAccident();
+    }
 }
