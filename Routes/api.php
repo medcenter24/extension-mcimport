2f07937fae01df786f8cae5dae01a848a280a2f2
MedCenter24 Dev <dev@medcenter24.com>
2019-04-25 16:00:53 +0300

    fixed import and modules

--- Routes/api.php
+++ Routes/api.php
@@ -1,6 +1,23 @@
 <?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2019 (original work) MedCenter24.com;
+ */
 
-use Illuminate\Http\Request;
+use Dingo\Api\Routing\Router;
+use medcenter24\McImport\Http\Controllers\Api\V1\Director\CasesImporterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +30,33 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/mcimport', function (Request $request) {
-    return $request->user();
+/** @var Router $api */
+$api = app(Router::class);
+
+$api->group([
+    'version' => 'v1',
+    'middleware' => 'api',
+    'prefix' => 'api',
+], static function (Router $api) {
+    $api->version('v1', ['middleware' => ['cors']], static function (Router $api) {
+        $api->group([
+            'middleware' => 'api.auth'
+        ], static function (Router $api) {
+
+            $api->group(['prefix' => 'director', 'middleware' => ['role:director']], static function (Router $api) {
+
+                $api->group(['prefix' => 'cases'], static function (Router $api) {
+
+                    $api->post('importer', CasesImporterController::class . '@upload');
+                    $api->get('importer', CasesImporterController::class . '@uploads');
+                    $api->put('importer/{id}', CasesImporterController::class . '@import');
+                    $api->delete('importer/{id}', CasesImporterController::class . '@destroy');
+
+                    /*$api->group(['prefix' => 'importer'], static function (Router $api) {
+
+                    });*/
+                });
+            });
+        });
+    });
 });
